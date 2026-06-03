1975ed291b28fac7b402d5143b36d2a00774748e
Tunan10 <tunansakib200@gmail.com>
2026-06-03 13:44:58 +0600

    Adding await before client function

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,6 +2,7 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 const client = new MongoClient(process.env.MONGODB_URI);
+await client.connect();
 const db = client.db('bookMart');
 export const auth = betterAuth({
   database: mongodbAdapter(db),
